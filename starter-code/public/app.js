986c51bd8082f7e6fcd466fafc694181e7a110c7
Brianna Veenstra <b.g.veenstra@gmail.com>
2016-08-29 11:58:57 -0700

    add starter code

---
+++ starter-code/public/app.js
@@ -0,0 +1,4 @@
+console.log('app.js connected');
+$(document).ready(function(){
+  console.log('DOM ready');
+});
---
+++ starter-code/server.js
@@ -0,0 +1,40 @@
+// SERVER-SIDE JAVASCRIPT
+// run npm install to install all required packages before starting server
+
+var express = require('express');
+var app = express();
+
+
+// MIDDLEWARE
+app.use(express.static('public'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Allow CORS:
+// not necessary since we'll be making requests from a js file
+  // that we are also serving (as static assets in public)
+// app.use(function(request, response, next) {
+//   response.header("Access-Control-Allow-Origin", "*");
+//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+// ROUTES
+// Root Route
+
+
+// Gallery View Route
+
+
+// The Number Guessing Game
+
+
+// Gallery
+
+
+// SERVER START
+var port = 3000;
+app.listen(port, function(){
+  console.log('Server Running at localhost:3000/');
+});
